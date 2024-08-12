6da06076b8417adf94f0fdcac2e7d4055345b00e
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-12 09:30:27 -0600

    metodo, constructor y un public void para calcular el total

--- Pedido.cs
+++ Pedido.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lab3
+﻿public class Pedido
 {
-    internal class Pedido
+    public int NumeroPedido { get; set; }
+    public DateTime Fecha { get; set; }
+    public Cliente Cliente { get; set; }
+    public List<Producto> Productos { get; set; }
+    public float Total { get; set; }
+
+    public Pedido(int numeroPedido, DateTime fecha, Cliente cliente)
     {
+        NumeroPedido = numeroPedido;
+        Fecha = fecha;
+        Cliente = cliente;
+        Productos = new List<Producto>();
+        Total = 0;
+    }
+
+    public void CalcularTotal()
+    {
+        Total = Productos.Sum(p => p.Precio);
+
+        if (Cliente is ClienteVIP vip)
+        {
+            Total -= Total * vip.Descuento / 100;
+        }
+        else if (Cliente is ClienteCorporativo corporativo)
+        {
+            Total -= Total * corporativo.Descuento / 100;
+        }
     }
 }
+
