d8980572c4f55744d7f07a7e044501ad4258d87a
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-08-12 09:34:18 -0600

    constructor de la tienda

--- Tienda.cs
+++ Tienda.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lab3
+﻿public class Tienda
 {
-    internal class Tienda
+    public List<Cliente> Clientes { get; set; }
+    public List<Vehiculo> Vehiculos { get; set; }
+    public List<Pedido> Pedidos { get; set; }
+
+    public Tienda()
     {
+        Clientes = new List<Cliente>();
+        Vehiculos = new List<Vehiculo>();
+        Pedidos = new List<Pedido>();
     }
 }
+
